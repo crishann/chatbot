3cfc37a597d3689d5d2662c204a4d84cc51c095d
crishann <cristiantorrejos03@gmail.com>
2025-12-19 21:41:53 +0800

    hero updated

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -2,6 +2,7 @@ export default function Hero() {
   return (
     <section id="home" className="container section">
       <h2>Hi, I’m a CRISTIAN TORREJOS</h2>
+      <h3>IT-ELAI FINALS</h3>
       <p style={{ maxWidth: "600px", marginTop: "1rem" }}>
         I'm a full stack web developer. I build scalable web applications and clean user experiences.
         I currently specialize in React for frontend development and Node for backend development.
